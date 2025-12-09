4ae4ccf799e493a5bf3c16bbe64e29332ef9e708
Dip Das <dipdastoko@gmail.com>
2025-12-09 13:05:30 +0600

    __name__ & __main__ explored

---
+++ name & main/one.py
@@ -0,0 +1,9 @@
+#one.py
+def func():
+    print("FUNC() IN ONE.PY")
+print("TOP LEVEL IN ONE.PY")
+
+if __name__ == "__main__":
+    print("One.py is being run directly!")
+else:
+    print("One.py has been imported")
